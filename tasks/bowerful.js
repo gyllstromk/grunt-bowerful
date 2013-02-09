f95fd443c0e30708057f1f0a7ef8bd20f5cbdfe9
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2013-02-09 10:34:17 -0600

    concat files that are implicit dependencies too

--- tasks/bowerful.js
+++ tasks/bowerful.js
@@ -7,6 +7,10 @@
  */
 
 module.exports = function(grunt) {
+    var path = require('path'),
+        util = require('util'),
+        fs = require('fs');
+
 
   // Please see the grunt documentation for more information regarding task and
   // helper creation: https://github.com/gruntjs/grunt/blob/master/docs/toc.md
@@ -16,70 +20,81 @@ module.exports = function(grunt) {
   // ==========================================================================
 
     grunt.registerTask('bowerful', 'Install bower packages.', function() {
-        var path = require('path'),
-            util = require('util'),
-            fs = require('fs');
-
         var done = this.async();
+        var deps = {};
+        var configs = {};
+
+        var bower = require('bower');
+        var config = require('bower/lib/core/config');
+        var Manager = require('bower/lib/core/manager');
+
+        var packages = grunt.config.get('bowerful.packages');
+        config.directory = grunt.config.get('bowerful.store') || 'components';
+        config.dest = grunt.config.get('bowerful.dest');
+
+        function buildConfig(packageName) {
+            if (deps[packageName]) {
+                return;
+            }
+
+            var base = path.join(config.directory, packageName.split('/').pop());
+            var json = grunt.file.readJSON(path.join(base, 'component.json'));
+
+            if (! json.main) {
+                grunt.log.error(util.format('Package %s did not specify a `main` file in components.json.', packageName));
+                grunt.log.error('Trying `package.json`');
+                var content = grunt.file.readJSON(path.join(base, 'package.json'));
+                json.main = content.main;
+            }
+
+            if (! json.main) {
+                grunt.log.error('Nothing in `package.json`. Reverting to guesswork based on package name.');
+                var guess = path.join(base, packageName + '.js');
+                if (fs.existsSync(guess)) {
+                    grunt.log.error(util.format('%s exists, assuming this is the correct file.', guess));
+                    json.main = guess;
+                } else {
+                    grunt.log.error(util.format('Cannot find main file for %s. Please install manually (or file bug report to %s with your grunt.js).', packageName, 'https://github.com/gyllstromk/grunt-bowerful/issues'));
+                }
+            }
+
+            if (! Array.isArray(json.main)) {
+                json.main = [ json.main ];
+            }
+
+            deps[packageName] = Object.keys(json.dependencies || []);
+            configs[packageName] = json;
+            deps[packageName].forEach(buildConfig);
+        }
 
         function installed(packages) {
             if (config.dest) {
                 var written = {};
-                var deps = {};
-                var configs = {};
                 var contents = {};
 
-                Object.keys(packages).forEach(function(package) {
-                    var base = path.join(config.directory, package.split('/').pop());
-                    var json = grunt.file.readJSON(path.join(base, 'component.json'));
-
-                    if (! json.main) {
-                        grunt.log.error(util.format('Package %s did not specify a `main` file in components.json.', package));
-                        grunt.log.error('Trying `package.json`');
-                        var content = grunt.file.readJSON(path.join(base, 'package.json'));
-                        json.main = content.main;
-                    }
-
-                    if (! json.main) {
-                        grunt.log.error('Nothing in `package.json`. Reverting to guesswork based on package name.');
-                        var guess = path.join(base, package + '.js');
-                        if (fs.existsSync(guess)) {
-                            grunt.log.error(util.format('%s exists, assuming this is the correct file.', guess));
-                            json.main = guess;
-                        } else {
-                            grunt.log.error(util.format('Cannot find main file for %s. Please install manually (or file bug report to %s with your grunt.js).', package, 'https://github.com/gyllstromk/grunt-bowerful/issues'));
-                        }
-                    }
-
-                    if (! Array.isArray(json.main)) {
-                        json.main = [ json.main ];
-                    }
-
-                    deps[package] = Object.keys(json.dependencies || {});
-                    configs[package] = json;
-                });
+                Object.keys(packages).forEach(buildConfig);
 
                 function write(packageName) {
-                    var package = configs[packageName];
-                    if (! package) {
+                    var pkg = configs[packageName];
+                    if (! pkg) {
                         return;
                     }
 
-                    (deps[package.name] || []).forEach(function(dep) {
+                    (deps[pkg.name] || []).forEach(function(dep) {
                         if (! written[dep]) {
                             write(dep);
                         }
                     });
 
-                    package.main.forEach(function(file) {
+                    pkg.main.forEach(function(file) {
                         var ext = path.extname(file);
                         if (! contents[ext]) {
                             contents[ext] = '';
                         }
 
-                        var base = path.join(config.directory, package.name);
+                        var base = path.join(config.directory, pkg.name);
 
-                        file = path.join(config.directory, package.name, file);
+                        file = path.join(config.directory, pkg.name, file);
 
                         if (! fs.existsSync(file)) {
                             grunt.log.error(file + ' not found. Skipping.');
@@ -88,7 +103,7 @@ module.exports = function(grunt) {
                         }
                     });
 
-                    written[package.name] = true;
+                    written[pkg.name] = true;
                 }
 
                 grunt.utils._.keys(configs).forEach(write);
@@ -99,17 +114,9 @@ module.exports = function(grunt) {
             }
 
             done();
-        };
+        }
 
         this.requiresConfig('bowerful.packages');
-
-        var bower = require('bower');
-        var config = require('bower/lib/core/config');
-        var Manager = require('bower/lib/core/manager');
-
-        var packages = grunt.config.get('bowerful.packages');
-        config.directory = grunt.config.get('bowerful.store') || 'components';
-        config.dest = grunt.config.get('bowerful.dest');
 
         // manager endpointNames argument requires inverted packages
         var invert = {};
